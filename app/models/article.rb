41e8025122f3aaf06743d547b4666257a9cfb6e5
iliastsangaris <iliastsangaris@gmail.com>
2015-01-12 20:12:17 -0800

    favoriting

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 	validates :title, presence: true
 	validates :body, 	presence: true
-
   belongs_to :user
+  has_many :favorites
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_many :favorites
+  # user.faves returns favorite articles
+  has_many :faves, through: :favorites, source: :article
 end
